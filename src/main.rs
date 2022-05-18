15fba231fe19a9d403ebbe46b890514c819618c5
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-05-18 21:44:45 +0800

    :sparkles: capture screen rgba data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use dxgcap::DXGIManager;
+
 fn main() {
-    println!("hello, deskmirror");
+    let screen = &mut DXGIManager::new(1000 * 60).unwrap();
+    let temp = screen.capture_frame().unwrap();
+    println!("each frame of the monitor has: {:?} bytes", temp.0.len());
+    println!("its resolution is: {:?} * {:?}", temp.1.0, temp.1.1);
 }
