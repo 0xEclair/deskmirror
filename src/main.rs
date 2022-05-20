77a7ffe17b6b909ccc6135aaa7e7d38decac4edf
eclair-lumiere <38656355+eclair-lumiere@users.noreply.github.com>
2022-05-21 01:12:44 +0800

    :zap: reduce copy cost

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
 #![feature(vec_into_raw_parts)]
 
 use iced::{Element, Settings, Column, Text, Alignment, image, Application, Subscription, Command};
-use dxgcap::{BGRA8, DXGIManager};
+use dxgcap::DXGIManager;
 
 struct ScreenViewer {
     size: (u32, u32),
-    screen: image::Handle,
     dxgi: DXGIManager
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Message {
+    Default(u8)
 }
 
 impl Application for ScreenViewer {
     type Executor = iced::executor::Default;
-    type Message = ();
+    type Message = Message;
     type Flags = ();
 
-    fn new(_: Self::Flags) -> (Self, Command<()>) {
+    fn new(_: Self::Flags) -> (Self, Command<Message>) {
         let mut dxgi_manager = DXGIManager::new(1000 * 60).unwrap();
         let temp = dxgi_manager.capture_frame().unwrap();
         let width = temp.1.0 as u32;
@@ -22,7 +26,6 @@ impl Application for ScreenViewer {
         (
             ScreenViewer{
                 size: (width, height),
-                screen: image::Handle::from_pixels(width, height, pixels_from(temp.0)),
                 dxgi: dxgi_manager
             },
             Command::none()
@@ -33,15 +36,19 @@ impl Application for ScreenViewer {
         String::from("deskmirror")
     }
 
-    fn update(&mut self, message: Self::Message) -> Command<()> {
+    fn update(&mut self, message: Self::Message) -> Command<Message> {
         match message {
             _ => {
 
             }
         }
-        let temp = self.dxgi.capture_frame().unwrap();
-        self.screen = image::Handle::from_pixels(self.size.0, self.size.1, pixels_from(temp.0));
         Command::none()
+    }
+
+    fn subscription(&self) -> Subscription<Self::Message> {
+        iced::time::every(std::time::Duration::from_millis(16)).map(|_|{
+            Message::Default(5)
+        })
     }
 
     fn view(&mut self) -> Element<'_, Self::Message> {
@@ -50,25 +57,17 @@ impl Application for ScreenViewer {
             .align_items(Alignment::Center)
             .push(Text::new(self.size.0.to_string()).size(50))
             .push(Text::new(self.size.1.to_string()).size(50))
-            .push(image::Image::new(self.screen.clone()))
+            .push(image::Image::new(
+                image::Handle::from_pixels(
+                    self.size.0,
+                    self.size.1,
+                    self.dxgi.capture_frame_components().unwrap().0
+                ))
+            )
             .into()
-    }
-
-    fn subscription(&self) -> Subscription<Self::Message> {
-        iced::time::every(std::time::Duration::from_millis(50)).map(|_|{
-            ()
-        })
     }
 }
 
 fn main() -> iced::Result {
     ScreenViewer::run(Settings::default())
-}
-
-fn pixels_from(vec: Vec<BGRA8>) -> Vec<u8> {
-    unsafe {
-        let(a, b, c) = vec.into_raw_parts();
-        let ptr = a as *mut u8;
-        Vec::from_raw_parts(ptr, 4*b, 4*c)
-    }
 }
